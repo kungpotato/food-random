c2c3ea6f2678e92eeec92d525945059e07a0a352
KUNGPOTATO\kungp <kittisak.py@gmail.com>
2023-09-28 21:05:07 +0700

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodrandom/manage.dart';
 import 'package:foodrandom/random.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
